11d1687c60c85f84e8781d46ba907ec3ccc80b67
oneadm <60870148+oneadms@users.noreply.github.com>
2022-03-24 02:29:03 +0800

    生命周期钩子

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-<div class="container">
-  <!-- <test :name="name" hello="true" >
+  <div class="container">
+    <!-- <test :name="name" hello="true" >
 
       <template  v-slot:slot1>
         <div >
@@ -17,45 +17,83 @@
       </template>
 
   </test> -->
-<mixin></mixin>
+    <mixin></mixin>
 
-</div>
+  </div>
 </template>
 
 <script>
 // import test from '@/components/test'
 import mixin from '@/components/mixin'
-import { onMounted,onBeforeMount } from '@vue/runtime-core'
+import {
+  onMounted,
+  onBeforeMount,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+  onActivated,
+  onDeactivated,
+  onErrorCaptured,
+  reactive,
+} from '@vue/runtime-core'
 export default {
   name: 'App',
-  components:{
-  mixin
+  components: {
+    mixin,
   },
-  setup(){
-    onMounted(()=>{
-      alert('hello')
-      
-    }),
-    onBeforeMount(()=>{
-      alert("达咩")
-    })
-
-    
+  setup() {
    
+    /**
+     * onBeforeMount——挂载开始前调用
+onMount——挂载后调用
+onBeforeUpdate——当响应数据改变，且重新渲染前调用
+onUpdated——重新渲染后调用
+onBeforeUnmount——Vue实例销毁前调用
+onUnmounted——实例销毁后调用
+onActivated——当keep-alive组件被激活时调用
+onDeactivated——当keep-alive组件取消激活时调用
+onErrorCaptured——从子组件中捕获错误时调用
+     */
+    // 生命周期 钩子 写在一块  形成一片独立的流程 看起来更加清晰
+    onBeforeMount(() => {
+      alert('1')
+    }),
+      onMounted(() => {
+        alert('2')
+       
+      }),
+      onBeforeUpdate(() => {
+        alert('3')
+      }),
+      onUpdated(() => {
+        alert('4')
+      }),
+      onBeforeUnmount(() => {
+        alert('5')
+      }),
+      onUnmounted(() => {
+        alert('6')
+      }),
+      onActivated(() => {
+        alert('7')
+      }),
+      onDeactivated(() => {
+        alert('8')
+      }),
+      onErrorCaptured(() => {
+        alert('9')
+      })
   },
-  data(){
-    return{
-      name:"hello"
+  data() {
+    return {
+      name: "hello"
     }
   },
- 
-  created(){
-  }
 }
 </script>
 
 <style>
-
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
